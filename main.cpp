a31d52fce5ee01b10fab618bf7dcf901af71d219
Anshul Kawarey <anshulkawarey@gmail.com>
2026-06-11 19:22:20 +0530

    Add camera class for rendering images
    - Introduced a new camera class to handle image rendering with specified aspect ratio and width.
    - Implemented the render method to generate pixel colors based on ray tracing.
    - Updated main.cpp to utilize the camera class for rendering the scene.
    - Included necessary header files in CMakeLists.txt and color.h for new functionality.

--- main.cpp
+++ main.cpp
@@ -4,31 +4,7 @@
 #include "hittable_list.h"
 #include "vec3.h"
 #include "color.h"
-
-color ray_color(const ray& r, const hittable& world){
-    hit_record rec;
-    if(world.hit(r, interval(0, infinity), rec)){
-        return 0.5 * (rec.normal + color(1, 1, 1));
-    }
-    
-   
-   
-    vec3 unit_direction = unit_vector(r.direction());
-    // Since unit_dir ranges from [-1,1] basically -1 being down and 1 being up,
-    // we need to shoft range from [-1,1] to [0,1]
-    // so we add 1 to unit_dir's y component (with intent of creating a vertical gradient, can take x if want horizontal gradient)
-    // then we divide by 2 since adding 1 makes range [0,2]
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    
-    // now we use this 'a' to create a gradient,
-    // to do that we return a color which will create a gradual shift between the 2 colors we want
-    // in this case we will do it from white to sky blue,
-    // i.e. (1, 1, 1) to (0.5, 0.7, 1.0)
-    // and we make the value using the formula
-    // (1-a)*color1 + a*color2
-    return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-
+#include "camera.h"
 
 
 int main(){
@@ -37,57 +13,21 @@ int main(){
     // y going down-up, 
     // z going into the screen
     
-    
-    // Image
-    auto aspect_ratio= 16.0 / 9.0;
-    int image_width = 400;
-
-    int image_height = int(image_width/aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
     // World
     hittable_list world;
-
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-
-    // Camera
-    auto focal_length = 1.0;
-    auto camera_center = point3(0,0,0);
     
-    // Viewport
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    // world.add(make_shared<sphere>(point3(0,10,-10), 0.5));
+    // world.add(make_shared<sphere>(point3(10,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    
+    camera cam;
+    // Image
+    cam.aspect_ratio= 16.0 / 9.0;
+    cam.image_width = 400;
+    
+    cam.render(world);
 
-    // Calc the vectors to go down and left-right on the Viewport (based on the global coordinate system)
-    auto viewport_u = vec3(viewport_width, 0 ,0); 
-    auto viewport_v = vec3(0 , -viewport_height ,0); 
-
-    // Calc the delta vectors using the viewport vectors and image dimensions
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calc the position of the top left pixel on the viewport
-    // Camera is aligned with the centre of the viewport and the focal length is how much out of the screen the Camera is placed. 
-    // So we subtract the distance we are out of the screen since the Z axis goes into the plane.
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - (viewport_u/2) - (viewport_v/2);
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render 
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-    for(int j = 0; j < image_height; j++){
-        std::clog << "\rScanning remaining: " << (image_height-j) << ' ' <<std::flush;
-        for(int i = 0; i < image_width; i++){
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center; // destination - source of 2 points gives a vector direction
-            ray r = ray(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-
-            write_color(std::cout, pixel_color);
-        }
-    }
+    
     std::clog << "\rDone.                          \n"; 
 }
--- rtinclude.h
+++ rtinclude.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -15,6 +16,16 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double(){
+    // Return a random real number in [0,1)
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max){
+    // Return a random real number in [0,1)
+    return min + (max-min) * random_double();
 }
 
 #endif
