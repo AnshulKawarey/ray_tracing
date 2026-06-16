51f5eacec61188d37ffefbeb39174b9aac2a864e
Anshul Kawarey <anshulkawarey@gmail.com>
2026-06-16 19:23:16 +0530

    Enhance camera class for improved rendering
    - Added support for multiple samples per pixel and maximum recursion depth in the camera class.
    - Updated the render method to incorporate anti-aliasing by averaging colors from multiple samples.
    - Introduced new methods for ray generation and random sampling within the camera class.
    - Adjusted main.cpp to configure the camera with higher resolution and increased sampling for better image quality.
    - Included necessary header files in camera.h and vec3.h for new functionalities.

--- camera.h
+++ camera.h
@@ -3,12 +3,16 @@
 
 #include "color.h"
 #include "hittable.h"
+#include "rtinclude.h"
+#include "vec3.h"
 
 class camera {
 public:
   // Image
   double aspect_ratio = 1.0;
   int image_width = 100;
+  int samples_per_pixel = 10;
+  int max_depth = 10;
 
   void render(const hittable &world) {
     initialize();
@@ -20,27 +24,30 @@ public:
       std::clog << "\rScanning remaining: " << (image_height - j) << ' '
                 << std::flush;
       for (int i = 0; i < image_width; i++) {
-        auto pixel_center =
-            pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-        auto ray_direction = pixel_center - center;
-        ray r = ray(center, ray_direction);
+        color pixel_color(0, 0, 0);
+        for(int sample = 0; sample < samples_per_pixel; sample++){
+          ray r = get_ray(i, j);
+          pixel_color += ray_color(r, max_depth, world);
+        }
 
-        color pixel_color = ray_color(r, world);
-        write_color(std::cout, pixel_color);
+        write_color(std::cout, pixel_samples_scale * pixel_color);
       }
     }
   }
 
 private:
-  int image_height;   // Rendered image height
-  point3 center;      // Camera center
-  point3 pixel00_loc; // Location of pixel 0, 0
-  vec3 pixel_delta_u; // Offset to pixel to the right
-  vec3 pixel_delta_v; // Offset to pixel below
+  int image_height;           // Rendered image height
+  point3 center;              // Camera center
+  point3 pixel00_loc;         // Location of pixel 0, 0
+  vec3 pixel_delta_u;         // Offset to pixel to the right
+  vec3 pixel_delta_v;         // Offset to pixel below
+  double pixel_samples_scale; // Color scale factor for a sum of pixel samples
 
   void initialize() {
-    int image_height = int(image_width / aspect_ratio);
+    image_height = int(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    pixel_samples_scale = 1.0 / samples_per_pixel;
 
     // Camera
     auto focal_length = 1.0;
@@ -69,11 +76,33 @@ private:
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
   }
 
-  color ray_color(const ray &r, const hittable &world) {
+  ray get_ray(int i, int j) const {
+    // generate camera originating rays spread randomly around the target i, j
+    auto offset = sample_square();
+    auto pixel_sample = pixel00_loc 
+                        + ((i + offset.x()) * pixel_delta_u) 
+                        + ((j + offset.y()) * pixel_delta_v);
+    
+    auto ray_origin = center;
+    auto ray_direction = pixel_sample - ray_origin;
+
+    return ray(ray_origin, ray_direction);
+  }
+
+  vec3 sample_square() const {
+    return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+  }
+
+  color ray_color(const ray &r, int depth, const hittable &world) const {
+    if(depth <= 0){
+      return color(0, 0, 0);
+    }
+
     hit_record rec;
 
     if (world.hit(r, interval(0, infinity), rec)) {
-      return 0.5 * (rec.normal + color(1, 1, 1));
+      vec3 direction = random_on_hemisphere(rec.normal);
+      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
--- main.cpp
+++ main.cpp
@@ -17,14 +17,14 @@ int main(){
     hittable_list world;
     
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    // world.add(make_shared<sphere>(point3(0,10,-10), 0.5));
-    // world.add(make_shared<sphere>(point3(10,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
     
     camera cam;
     // Image
     cam.aspect_ratio= 16.0 / 9.0;
-    cam.image_width = 400;
+    cam.image_width = 800;
+    cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
     
     cam.render(world);
 
